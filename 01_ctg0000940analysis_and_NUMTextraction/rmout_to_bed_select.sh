ea30420201b87d0aea93212d900b95ec14199be3
mmontonerin <merce.montoliu@gmail.com>
2023-07-05 13:47:06 +0200

    Update script repeats

--- 01_ctg0000940analysis_and_NUMTextraction/rmout_to_bed_select.sh
+++ 01_ctg0000940analysis_and_NUMTextraction/rmout_to_bed_select.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
+awk '{if ($9 == "+") print $5, $6, $7, $11 "-" $10, $2, "+"; else print $5, $6, $7, $11 "-" $10, $2, "-";}' O11.upper.fasta.out | grep -e "ctg" > O11_all_repeats.bed
 
-#awk '{if ($9 == "+") print $5, $6, $7, $11 "-" $10, $2, "+"; else print $5, $6, $7, $11 "-" $10, $2, "-";}' O11.upper.fasta.out | grep -e "ctg" > O11_all_repeats.bed
+grep -e "ctg000940" O11_all_repeats.bed > O11_940_all_rep.bed
 
-#grep -e "rnd-5_family-2487" O11_all_repeats.bed > O11_mito_rep.bed
+grep -e "Unknown" O11_940_all_rep.bed > O11_940_unknown_rep.bed
 
-#grep -e "RC/Helitron" O11_all_repeats.bed > O11_helitron_rep.bed
+grep -e "RC/Helitron" O11_940_all_rep.bed > O11_940_helitron_rep.bed
 
-#grep -v "rnd-5_family-2487" O11_all_repeats.bed > O11_repeats_without_mito.bed
+grep -e "LTR/Gypsy" O11_940_all_rep.bed > O11_940_gypsy_rep.bed
 
-#grep -v "RC/Helitron" O11_repeats_without_mito.bed > O11_repeats_without_mitoANDheli.bed
+grep -e "DNA/hAT" O11_940_all_rep.bed > O11_940_hAT_rep.bed
 
-#grep -e "rnd-6_family-6103" O11_all_repeats.bed > O11_mito_rep2.bed
+grep -e "Simple_repeat" O11_940_all_rep.bed > O11_940_simple_rep.bed
 
-grep -e "LTR/Gypsy" O11_all_repeats.bed > O11_gypsy_rep.bed
+#To check what is left (repeats not relevant for the region in study (730kb-end))
+grep -v -E "RC/Helitron|Unknown|LTR/Gypsy|DNA/hAT|Simple_repeat" O11_940_all_rep.bed > O11_940_without_specials.bed
 
-grep -e "DNA/hAT" O11_all_repeats.bed > O11_hAT_rep.bed
-
-grep -v -E "RC/Helitron|rnd-5_family-2487|rnd-6_family-6103|LTR/Gypsy|DNA/hAT" O11_all_repeats.bed > O11_all_without_specials.bed
